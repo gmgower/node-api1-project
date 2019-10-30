d2f0354e9298ce5898b6bcfb2a4f24b3fa31ea99
Mada Gower <gmgower@gmail.com>
2019-10-30 18:20:45 -0500

    Setup up server listening port 8000.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.listen(8000, () => {
+    console.log('=== server listening on port 8000 ===');
+});
+
+
