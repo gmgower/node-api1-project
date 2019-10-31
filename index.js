f362c230450b6f772e43f02cd2dde9a636ed63a0
Mada Gower <gmgower@gmail.com>
2019-10-30 21:37:25 -0500

    Refactor POST request to /api/users/:id.

--- index.js
+++ index.js
@@ -20,19 +20,32 @@ server.get('/', (req, res) =>{
 // ! POST request to /api/users
 server.post('/api/users', (req, res) => {
     const userInfo = req.body;
+    const {name, bio} = req.body;
     console.log('body', userInfo)
 
-    dbUser.insert(userInfo)
-        .then(user => {
-            if(user){
-            res.status(201).json({success: true, user})
-            } else {
+    // dbUser.insert(userInfo)
+    //     .then(user => {
+    //         if(user){
+    //         res.status(201).json({success: true, user})
+    //         } else {
+    //         res.status(400).json({success: false, errorMessage: "Please provide name and bio for the user."})
+    //         }
+    //     })
+    //     .catch(err => {
+    //         res.status(500).json({error: "There was an error while saving the user to the database.", err})
+    //     })
+
+    if(!name || !bio) {
             res.status(400).json({success: false, errorMessage: "Please provide name and bio for the user."})
-            }
-        })
+    } else {
+        dbUser.insert(userInfo)
+            .then(user => {
+            res.status(201).json({success: true, user})
+            })
         .catch(err => {
             res.status(500).json({error: "There was an error while saving the user to the database.", err})
         })
+        }
 })
 
 
@@ -68,6 +81,7 @@ server.get('/api/users/:id', (req, res) => {
 server.put('/api/users/:id', (req, res) => {
     const {id} = req.params;
     const userInfo = req.body;
+    const {name, bio} = req.body;
 
     dbUser.update(id, userInfo)
         .then(user => {
