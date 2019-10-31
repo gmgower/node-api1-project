3ddb00ee364889cdceece16dd8d340c13ef72157
Mada Gower <gmgower@gmail.com>
2019-10-30 20:56:43 -0500

    PUT request to /api/users/:id.

--- index.js
+++ index.js
@@ -64,4 +64,21 @@ server.get('/api/users/:id', (req, res) => {
         })
     })
 
+// ! PUT request to /api/users/:id
+server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    const userInfo = req.body;
+
+    dbUser.update(id, userInfo)
+        .then(user => {
+            if(user) {
+                res.status(200).json({success: true, user})
+            } else {
+                res.status(404).json({success: false, message: `The user with the specified ID ${id} does not exist.`})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: "The user information could not be modified.", err})
+        })
+})
 
