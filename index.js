1a81e0794c6afa88794d5f8fa8f63b512b43bcc7
Mada Gower <gmgower@gmail.com>
2019-10-30 19:04:42 -0500

    GET request to /api/users.

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ const express = require('express');
 
 const server = express();
 
+const db = require('./data/db')
+
 server.listen(8000, () => {
     console.log('=== server listening on port 8000 ===');
 });
 
+server.get('/', (req, res) =>{
+    console.log('Testing get')
 
+    res.send('hello world...')
+})
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({error: "The users information could not be retrieved.", err})
+        })
+
+})
